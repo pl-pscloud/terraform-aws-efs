5c731a3f4482ecc888ec1308193331c13b7a669c
pswitala <p.switala@pslab.eu>
2020-01-19 09:50:14 +0100

    Changed default value for var lifecycle policy to null

--- vars.tf
+++ vars.tf
@@ -5,4 +5,4 @@ variable "pscloud_sec_gr_ids" {}
 variable "pscloud_subnets_ids" {}
 variable "pscloud_purpose" {}
 
-variable "pscloud_lifecycle_policy" { default = "AFTER_14_DAYS" }
+variable "pscloud_lifecycle_policy" { default = "" }
